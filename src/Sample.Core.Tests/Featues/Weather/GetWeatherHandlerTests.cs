81f19b1332288397b79536334d6433ee249153e7
Brendan Kowitz <bkowitz@microsoft.com>
2022-06-08 10:41:52 -0400

    Adds test

---
+++ src/Sample.Core.Tests/Featues/Weather/GetWeatherHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Sample.Core.Features.Weather;
+using Sample.Core.Messages.Get;
+using Xunit;
+
+namespace Sample.Core.Tests.Featues.Weather;
+
+public class GetWeatherHandlerTests
+{
+    private readonly GetWeatherHandler _handler;
+
+    public GetWeatherHandlerTests()
+    {
+        _handler = new GetWeatherHandler();
+    }
+
+    [Fact]
+    public async Task GivenAValidRequest_WhenRequestingWeather_ThenWeatherForecastsAreReturned()
+    {
+        GetWeatherResponse response = await _handler.Handle(new GetWeatherRequest(), CancellationToken.None);
+
+        Assert.NotEmpty(response.WeatherForecasts);
+    }
+}
